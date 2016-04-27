4e94e52f621b612053cfbbbcee2b4becfd80cbb5
rsamrat073 <rsamrat073@gmail.com>
2016-04-27 18:55:28 +0530

    Adding the Project

---
+++ MailHelper/src/com/hcl/MailSender.java
@@ -0,0 +1,72 @@
+package com.hcl;
+
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class MailSender {
+
+	public static void main(String[] args) {
+		final String username = "rsamrat073@gmail.com";
+		final String password = "9!0@9#0$*4#SA";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+	        message.setFrom(new InternetAddress("rsamrat073@gmail.com"));
+	        message.setRecipients(Message.RecipientType.TO,
+	                InternetAddress.parse("rsamrat073@gmail.com"));
+	        message.setSubject("Testing Subject");
+	        message.setText("PFA");
+
+	        MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+	        Multipart multipart = new MimeMultipart();
+
+	        messageBodyPart = new MimeBodyPart();
+	        String file = "D:\\KRANTI_WS\\MailHelper\\src\\com\\hcl\\MailSender.java";
+	        String fileName = "attachmentName";
+	        DataSource source = new FileDataSource(file);
+	        messageBodyPart.setDataHandler(new DataHandler(source));
+	        messageBodyPart.setFileName(fileName);
+	        multipart.addBodyPart(messageBodyPart);
+
+	        message.setContent(multipart);
+
+	        System.out.println("Sending");
+
+	        Transport.send(message);
+
+	        System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
